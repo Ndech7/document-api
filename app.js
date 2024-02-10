85610eb0f65699b4a89afdef7d02a48dfc91df51
Ndech7 <stephennderitu3@gmail.com>
2024-02-10 18:03:03 +0300

    Document the API with Swagger

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 import express from "express";
 import cors from "cors";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
 
 import libraryRoutes from "./library/routes/library.routes.js";
 
 const app = express();
 const port = 3000;
 
+// swagger definiton
+const swaggerSpec = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: `http://localhost:${port}`,
+      },
+    ],
+  },
+  apis: ["./library/routes/*.js"],
+};
+
 // Global Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerJSDoc(swaggerSpec))
+);
 
 // Routes
 app.use("/library", libraryRoutes);
--- library/routes/library.routes.js
+++ library/routes/library.routes.js
@@ -9,18 +9,151 @@ import {
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *     Book:
+ *      type: object
+ *      properties:
+ *          id:
+ *              type: integer
+ *              description: Book id
+ *          name:
+ *              type: string
+ *              description: Book name
+ *          author:
+ *              type: integer
+ *              description: Book author
+ *          description:
+ *              type: string
+ *              description: Book description
+ *          edition:
+ *              type: string
+ *              description: Book edition
+ *     example:
+ *          id: 2,
+            name: "PostGIS In Action"
+            author: "Regina O. Obe"
+            description: "An Introduction to PostGIS"
+            edition: "3rd"
+ */
+
+/**
+ * @swagger
+ * /library:
+ *  get:
+ *     summary: Get all books
+ *     description: Get all books
+ *     responses:
+ *      200:
+ *         description: Success
+ *      500:
+ *         description: Internal Server Error
+ */
 // get all books
 router.get("/", getBooks);
 
+/**
+ * @swagger
+ * /library/{id}:
+ *  get:
+ *     summary: Get book detail
+ *     description: Get book detail
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Book id
+ *     responses:
+ *      200:
+ *         description: Success
+ *      500:
+ *         description: Internal Server Error
+ */
 // get a single book by its id
 router.get("/:id", getBook);
 
+/**
+ * @swagger
+ * /library/{id}:
+ *   put:
+ *     summary: Edit book
+ *     description: Edit book
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Book id
+ *     requestBody:
+ *       description: A JSON object containing book information
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *           example:
+ *             name: "PostGIS In Action"
+ *             author: "Regina O. Obe"
+ *             description: "An Introduction to PostGIS"
+ *             edition: "3rd"
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Internal Server Error
+ */
 // update a book by its id
 router.put("/:id", updateBook);
 
+/**
+ * @swagger
+ * /library:
+ *   post:
+ *     summary: Add book
+ *     description: Add book
+ *     requestBody:
+ *       description: A JSON object containing book information
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *           example:
+ *             name: "PostGIS In Action"
+ *             author: "Regina O. Obe"
+ *             description: "An Introduction to PostGIS"
+ *             edition: "3rd"
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Internal Server Error
+ */
 // add a new book
 router.post("/", addBook);
 
+/**
+ * @swagger
+ * /library/{id}:
+ *  delete:
+ *     summary: Delete book
+ *     description: Delete book
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Book id
+ *     responses:
+ *     200:
+ *        description: Success
+ *     500:
+ *       description: Internal Server Error
+ */
 // delete a book by its id
 router.delete("/:id", deleteBook);
 
